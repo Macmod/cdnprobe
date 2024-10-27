df76270f3cc8e633e67997640747dd02aa4c02d6
Macmod <zz.mcmd@gmail.com>
2024-10-27 18:50:37 -0300

    Improving output.

--- cdnprobe.sh
+++ cdnprobe.sh
@@ -6,9 +6,9 @@ URLPATH="${2:-}"
 SCHEMA="${3:-https}"
 UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0'
 
-echo "[+] Trying to find $SCHEMA://$URLDOMAIN$URLPATH"
+# echo "[+] Trying to find $SCHEMA://$URLDOMAIN$URLPATH"
 
 jq -rc 'to_entries[] | "\(.key),\(.value[])"' cdnips.json | while IFS="," read provider cdnip; do
     result=$(httpx -no-stdin -u "$SCHEMA://$cdnip$URLPATH" -sni "$URLDOMAIN" -H "Host: $URLDOMAIN" -H "User-Agent: $UA" $HTTPXFLAGS 2>&1 | tail -n 1)
-    echo -e "\e[34m[$provider $cdnip]\e[0m $result"
+    echo -e "\e[34m$URLDOMAIN $provider $cdnip\e[0m $result"
 done
